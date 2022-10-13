08161ff0f38220ac4bae81b2107fd1f9e06f64c8
sarvar <tolibbeknur2@gmail.com>
2022-10-13 19:30:00 +0500

    fourth commit

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['codeblog-research.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
